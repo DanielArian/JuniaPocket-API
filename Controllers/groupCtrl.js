1e6f8cb1e1747d690fc45d7bcdd381126906b150
DanielArian <Daniel@DESKTOP-VKO5OCA>
2021-06-20 19:11:42 +0200

    -

--- Controllers/groupCtrl.js
+++ Controllers/groupCtrl.js
@@ -3,6 +3,7 @@ const db = require('../Database/index');
 const sCode = require('../httpStatus');
 
 exports.createGroup = async function (req, res) {
+
     console.log(req.body);
     res.set('Content-Type', 'application/json');
 
@@ -30,7 +31,7 @@ exports.getUserGroups = async function (req, res) {
     try {
         var liste = await db.Models.Group.find();
     } catch (error) {
-        console.log(`/leave-group error --> ${error}`);
+        console.log(`getUserGroups error --> ${error}`);
         return res.status(sCode.serverError).json({ error: 'SERVER_ERROR' });
     }
     var clearList = liste.filter(item => item.list.includes(aurionID));
@@ -41,23 +42,56 @@ exports.getUserGroups = async function (req, res) {
 
 exports.joinGroup = async function (req, res) {
 
-    
-}
 
+    if (!req.body.hasOwnProperty('aurionIDToAdd') || !req.body.hasOwnProperty('groupID')) {
+        return res.status(sCode.badRequest).json({error: 'Au moins un paramètre manquant.'})
+    }
+
+    let aurionIDToAdd = req.body.aurionIDToAdd;
+    let groupID = req.body.groupID;
+
+    let groupDoc;
+    try {
+        groupDoc = await db.Models.Group.findOne({'_id': mongoose.Types.ObjectId(groupID)})
+    } catch (error) {
+        console.log(`joinGroup error --> ${error}`);
+        return res.status(sCode.serverError).json({ error: 'SERVER_ERROR' });
+    }
+
+    if (groupDoc.list.includes(aurionIDToAdd)) {
+        return res.status(sCode.conflict).json({message: "L'utilisateur est déjà dans ce groupe."});
+    }
+
+
+    try {
+        await db.Models.Group.updateOne(
+            { '_id': mongoose.Types.ObjectId(groupID) },
+            { $push: { "list": aurionIDToAdd } });
+        return res.status(200).json({message: 'Utilisateur ajouté avec succès'});
+    } catch (error) {
+        console.log(`joinGroup error --> ${error}`);
+        return res.status(sCode.serverError).json({ error: 'SERVER_ERROR' });
+    }
+}
 
 
 exports.leaveGroup = async function (req, res) {
 
     let aurionID = req.user.aurionID;       // assuré par auth.js
 
+    if (!req.body.hasOwnProperty('groupID')) {
+        return res.status(sCode.badRequest).json({error: 'Au moins un paramètre manquant.'})
+    }
+    let groupID = req.body.groupID;
+
     res.set('Content-Type', 'application/json');
     try {
         await db.Models.Group.updateOne(
-            { '_id': mongoose.Types.ObjectId(req.body.groupId) },
+            { '_id': mongoose.Types.ObjectId(groupID) },
             { $pull: { "list": aurionID } });
         return res.status(200).json({message: 'Suppression ok'});
     } catch (error) {
-        console.log(`/leave-group error --> ${error}`);
+        console.log(`leaveGroup error --> ${error}`);
         return res.status(sCode.serverError).json({ error: 'SERVER_ERROR' });
     }
 }
--- Routes/group.js
+++ Routes/group.js
@@ -9,6 +9,7 @@ router.get('/get', groupCtrl.getUserGroups);
 // AJOUTER ICI UN MIDDLEWARE POUR VERIF QUE LE GROUP ID EST BIEN FOURNI, sinon BAD REQUEST
 
 router.post('/create', groupCtrl.createGroup);
+router.post('/join', groupCtrl.joinGroup);
 router.post('/leave', groupCtrl.leaveGroup);
 
 module.exports = router;
