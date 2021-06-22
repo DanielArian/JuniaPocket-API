9a81b3532cf88e6b71b0e1b016f699a005cd6cfb
daniel <daniel.arian@isen.yncrea.fr>
2021-06-23 01:03:19 +0200

    -

--- Controllers/widgetCtrl.js
+++ Controllers/widgetCtrl.js
@@ -126,7 +126,9 @@ exports.setHabitsWidget = async function (req, res) {
     
     let aurionID = req.user.aurionID;
     let widgetName = req.body.widgetName;       // a verif dans MIDDLEWARE
-    let hour = new Date().getUTCHours() + 2;
+    let dateNow = new Date();
+    let hour = new Date(  dateNow.setHours(dateNow.getHours() + 2)).getHours();
+    console.log('setHabitsWidget --> hour =', hour)
 
     let doc = await db.Models.Widget.findOne({ aurionID: aurionID })
     const oldValueHabits = doc.habits[hour][widgetName]
